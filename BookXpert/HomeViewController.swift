c5b8f78dd1e5939097c131489f57878e51ce61d3
laxxxx <lakshmancool9@gmail.com>
2025-04-17 12:19:35 +0530

    add logout button

--- BookXpert/HomeViewController.swift
+++ BookXpert/HomeViewController.swift
@@ -6,13 +6,15 @@
 //
 
 import UIKit
+import FirebaseAuth
+import GoogleSignIn
 
 class HomeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("HomeViewController loaded")
-
+        
     }
     
     @IBAction func didTapViewPDF(_ sender: UIButton) {
@@ -20,7 +22,7 @@ class HomeViewController: UIViewController {
         guard let pdfVC = storyboard.instantiateViewController(withIdentifier: "PDFViewerViewController") as? PDFViewerViewController else {
             return
         }
-
+        
         pdfVC.pdfURLString = "https://fssservices.bookxpert.co/GeneratedPDF/Companies/nadc/2024-2025/BalanceSheet.pdf"
         pdfVC.modalPresentationStyle = .fullScreen
         present(pdfVC, animated: true, completion: nil)
@@ -37,6 +39,43 @@ class HomeViewController: UIViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         if let imageVC = storyboard.instantiateViewController(withIdentifier: "ProductViewController") as? ProductViewController {
             present(imageVC, animated: true)
+        }
+    }
+    
+    @IBAction func didTapLogout(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Logout", message: "Are you sure you want to logout?", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        alert.addAction(UIAlertAction(title: "Logout", style: .destructive, handler: { _ in
+            self.performLogout()
+        }))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func performLogout() {
+        do {
+            // Firebase sign out
+            try Auth.auth().signOut()
+            
+            // Google sign out
+            GIDSignIn.sharedInstance.signOut()
+            
+            // Navigate to login screen (root view controller)
+            if let navController = self.navigationController {
+                navController.popToRootViewController(animated: true)
+            } else {
+                // Fallback in case HomeVC was presented modally
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                if let loginVC = storyboard.instantiateViewController(withIdentifier: "ViewController") as? ViewController {
+                    loginVC.modalPresentationStyle = .fullScreen
+                    present(loginVC, animated: true, completion: nil)
+                }
+            }
+            
+        } catch let signOutError {
+            print("Error signing out: \(signOutError.localizedDescription)")
         }
     }
 }
